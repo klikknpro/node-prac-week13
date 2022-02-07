1ce49d725e0f5d185b31096589d842a40075d4c8
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-07 18:52:43 +0100

    A little recap, notes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const app = express();
+const app = express(); // top-level express() function exported by the Express module
 const port = 3000;
 
+const add = (x, y) => {
+  if (x + y > 10) return "sok";
+  if (x + y < 0) return 0;
+  return Math.floor(x + y);
+};
+
+// app.get - Routes HTTP GET requests to the specified path with the specified callback functions.
 app.get("/", (req, res) => {
   const result = add(Number(req.query.num1), Number(req.query.num2));
   res.json(result);
@@ -11,8 +18,8 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const add = (x, y) => {
-  if (x + y > 10) return "sok";
-  if (x + y < 0) return 0;
-  return Math.floor(x + y);
-};
+/* simple GET example
+app.get('/', function (req, res) {
+  res.send('GET request to homepage')
+})
+*/
